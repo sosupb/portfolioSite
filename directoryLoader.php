6791f9d5be60264fb1556e1984be2928024e4176
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-23 14:37:34 -0800

    added directory loader file.

---
+++ directoryLoader.php
@@ -0,0 +1,12 @@
+<?php
+    echo "\$_SERVER['DOCUMENT_ROOT']: ";
+    echo $_SERVER['DOCUMENT_ROOT'] . "</br>";
+    
+    echo "getcwd(): ";
+    echo getcwd() . "</br>";
+    
+    echo "__DIR__: ";
+    echo __DIR__ . "</br>";
+    
+    
+?>

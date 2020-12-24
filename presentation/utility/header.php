acdeb4f332c351875fd9d5ac2d1ecd2bf8b13ba0
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-24 11:13:35 -0800

    Added more exception catching to unique counter code.

--- presentation/utility/header.php
+++ presentation/utility/header.php
@@ -4,19 +4,35 @@
     session_start();
 
     if(!isset($_SESSION['counter'])) {
-        $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/resources/default/counter.txt", "r");
-        if(is_null($file)){
-            ActivityLogger::warning("Could not open file for counter!");
+        
+        $counter = 0;
+        
+        if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/resources/default/counter.txt", "r")) {
+            try {
+                $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/resources/default/counter.txt", "r");
+                if(is_null($file)){
+                    ActivityLogger::warning("Counter file came back NULL!");
+                } else {
+                    $counter = (int)fread($file,20);
+                    fclose($file);
+                    $counter++;
+                    ActivityLogger::info("Updated unique visit counter to: " . $counter . "!");
+                    $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/resources/default/counter.txt", "w");
+                    fwrite($file, $counter);
+                    fclose($file);
+                }
+            }
+            catch (Exception $e) {
+                ActivityLogger::warning("There was a problem while attempting to access unique counter! " . $e->getMessage());
+                $counter = 0;
+            }
         } else {
-            $counter = (int)fread($file,20);
-            fclose($file);
-            $counter++;
-            ActivityLogger::info("Updated unique visit sounter to: " . $counter . "!");
+            $counter = 1;
             $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/resources/default/counter.txt", "w");
             fwrite($file, $counter);
             fclose($file);
-            $_SESSION['counter'] = $counter;
         }
+        $_SESSION['counter'] = $counter;
     }
 
     $navService = new NavBarService();

0a0c1f3314703256aa2e3e7e07e1ec05af2490c5
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-24 12:16:39 -0800

    added profile card file.

---
+++ presentation/Layouts/_profileCard.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This is a partial file to display a profile card on the main page.
+ */
+
+//must include library in header <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+?>
+
+<div class="profileCard">
+    <img src="headShot.jpg" alt="Marc" style="width:100%">
+    <h1>Marc Teixeira</h1>
+    <p class="profileTitle">title stuff</p>
+    <p>stuff</p>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <a href="#"><i class="fa fa-linkedin"></i></a>
+    <a href="#"><i class="fa fa-facebook"></i></a>
+    <p><button>Contact</button></p>
+</div>
+
+
+

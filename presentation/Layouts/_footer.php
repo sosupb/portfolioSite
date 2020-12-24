30bc18597a10405ef87ea25c7cc57b2a9c6425dc
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-23 23:27:58 -0800

    Added unique vist counter.

--- presentation/Layouts/_footer.php
+++ presentation/Layouts/_footer.php
@@ -3,6 +3,7 @@
 
 		<div class="fixedFooter">
 			<h2>Portfolio site for Marc D. Teixeira Jr. &emsp; Site up since: 12/22/2020</h2>
+			<h3 style="float: left;">Unique vists: <?=$_SESSION['counter']?></h3>
 		</div>
 	</body>
 </html>
--- presentation/utility/header.php
+++ presentation/utility/header.php
@@ -3,10 +3,20 @@
 
     session_start();
 
-    if(isset($_SESSION['visited'])) {
-    
-    } else {
-        $_SESSION['visited'] =  1;
+    if(!isset($_SESSION['counter'])) {
+        $file = fopen("counter.txt", "r");
+        if(is_null($file)){
+            ActivityLogger::warning("Could not open file for counter!");
+        } else {
+            $counter = (int)fread($file,20);
+            fclose($file);
+            $counter++;
+            ActivityLogger::info("Updated unique visit sounter to: " . $counter . "!");
+            $file = fopen("counter.txt", "w");
+            fwrite($file, $counter);
+            fclose($file);
+            $_SESSION['counter'] = $counter;
+        }
     }
 
     $navService = new NavBarService();

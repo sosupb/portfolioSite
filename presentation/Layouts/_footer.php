65908dae04f005d5003d01fbbd1eb3026d6875bf
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-24 11:25:00 -0800

    Fixed spelling error on footer.

--- presentation/Layouts/_footer.php
+++ presentation/Layouts/_footer.php
@@ -3,7 +3,7 @@
 
 		<div class="fixedFooter">
 			<h2>Portfolio site for Marc D. Teixeira Jr. &emsp; Site up since: 12/22/2020</h2>
-			<h3>Unique vists: <?=$_SESSION['counter']?></h3>
+			<h3>Unique visits: <?=$_SESSION['counter']?></h3>
 		</div>
 	</body>
 </html>

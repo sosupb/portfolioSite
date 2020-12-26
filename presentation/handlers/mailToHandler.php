54008e6d6327fc722c17b6db80cd28cd71b1fff4
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-26 15:04:36 -0800

    Fixed domain name in mailgun.

--- presentation/handlers/mailToHandler.php
+++ presentation/handlers/mailToHandler.php
@@ -28,7 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         $httpClient = new HttpClientConfigurator();
         $httpClient->setApiKey('04a9c293ecb17f6b5dce7d96fdc09f00-b6190e87-2f09670a');
         $mgClient = new Mailgun($httpClient);
-        $domain = "https://api.mailgun.net/v3/sandbox73434ced47974576958f2c94c24fdc54.mailgun.org";
+        $domain = "sandbox73434ced47974576958f2c94c24fdc54.mailgun.org";
         
         $results = $mgClient->messages()->send($domain, array(
             'from' => $_POST['email'],

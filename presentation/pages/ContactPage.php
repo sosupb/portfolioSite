dde38f43d90212fd0d8c45b87c017bcb01628fc8
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-26 10:59:02 -0800

    Fromat changes to contact form.

--- presentation/pages/ContactPage.php
+++ presentation/pages/ContactPage.php
@@ -10,9 +10,9 @@
 		<h1>Contact</h1>
 		<form action="mailToHandler.php" method="post">
 			<label for="name">Name</label>
-			<input id="name" type="text" name="name">
+			<input id="name" type="text" name="name"><br/>
 			<label for="email">Email</label>
-			<input id="email" type="text" name="email">
+			<input id="email" type="text" name="email"><br/>
 			<label for="message">Message</label>
 			<textarea id="message" name="message" rows="6" cols="25"></textarea><br/>
 			<input type="submit" value="Send"><input type="reset" value="Reset Form">

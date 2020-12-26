0b40203e48b45d7a726e30b24e41721571007e67
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-26 14:23:53 -0800

    Additional mailing fixes.

--- presentation/handlers/mailToHandler.php
+++ presentation/handlers/mailToHandler.php
@@ -17,7 +17,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $results = false;
     
     try {
-        $mgClient = Mailgun::create('04a9c293ecb17f6b5dce7d96fdc09f00-b6190e87-2f09670a');
+        $httpClient = new HttpClientConfigurator();
+        $httpClient->setApiKey('04a9c293ecb17f6b5dce7d96fdc09f00-b6190e87-2f09670a');
+        $mgClient = new Mailgun($httpClient);
         $domain = "https://api.mailgun.net/v3/sandbox73434ced47974576958f2c94c24fdc54.mailgun.org";
         
         $results = $mgClient->sendMessage($domain, array(

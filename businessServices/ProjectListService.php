efc62172a24dd07249ba7bacf2d4106b8561eb66
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 13:07:12 -0800

    Added panel for store app.

--- businessServices/ProjectListService.php
+++ businessServices/ProjectListService.php
@@ -12,7 +12,7 @@ class ProjectListService {
 		array_push($this->projects, Project::settingConstructor("Dark Current", "/resources/default/img/darkCurrentThumbnail.png", "C#, XNA Studio", "Visual Studio, ", 
 		                                                        "2015", "2D, Top down shooter.", "/resources/default/img/darkCurrentPanel.png"));
 		array_push($this->projects, Project::settingConstructor("Online Store Application", "/resources/default/img/storeAppThumbnail.png", "PHP, HTML, CSS, SQL", "CI/CD, git, backend design", "2018", 
-										"An online grocery store. Users can register or login, add to thier carts and checkout purchases.", "/resources/default/img/storeAppThumbnail.png"));
+										"An online grocery store. Users can register or login, add to thier carts and checkout purchases.", "/resources/default/img/storeAppPanel.png"));
 		array_push($this->projects, Project::settingConstructor("Music Album Library Application", "/resources/default/img/albumLibraryThumbnail.png", "PHP, HTML, CSS, SQL", "CI/CD, git, backend design", "2020", 
 										"Web application for users to store music album information. "
 		    . "Users are allowed to create an account and add/rate albums in their library.", "/resources/default/img/albumLibraryThumbnail.png"));

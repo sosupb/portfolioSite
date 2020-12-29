d924167f09fbc5e14621af0e531d9b3ab626be67
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 16:12:38 -0800

    Added content to the about me page.

--- presentation/pages/AboutPage.php
+++ presentation/pages/AboutPage.php
@@ -7,6 +7,12 @@
 
 	<div class="content">
 		<h1>About Me</h1>
+		
+		<h2>Technical Skills:</h2><br/>
+		<b>Programming Languages - </b><p>C#, C++, Java, PHP, JavScript, SQL, HTML, CSS</p><br/>
+		<b>Development Tools - </b><p>Visual Studio, Eclipse, GIT, MYSQLWorkbench, CI/CD, Cloud Development</p><br/>
+		
+		
 	</div>
 
 <?php 

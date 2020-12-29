3bd8cdf1505b6eee750b1b39209cb74327b4c7c8
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 16:19:00 -0800

    Added more information to the about page.

--- presentation/pages/AboutPage.php
+++ presentation/pages/AboutPage.php
@@ -7,11 +7,17 @@
 
 	<div class="content">
 		<h1>About Me</h1>
-		
-		<h2>Technical Skills:</h2><br/>
-		<b>Programming Languages - </b><p>C#, C++, Java, PHP, JavScript, SQL, HTML, CSS</p><br/>
-		<b>Development Tools - </b><p>Visual Studio, Eclipse, GIT, MYSQLWorkbench, CI/CD, Cloud Development</p><br/>
-		
+		<div style="margin-left: 3%;">
+    		<h2>Technical Skills:</h2>
+    		<b>Programming Languages - </b><p>C#, C++, Java, PHP, JavScript, SQL, HTML, CSS</p><br/>
+    		<b>Development Tools - </b><p>Visual Studio, Eclipse, GIT, CI/CD, Cloud Development, MySQLWorkbench,</p><br/>
+		</div>
+		<div style="margin-left: 3%;">
+    		<h2>Education:</h2>
+    		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming</p></br>
+    		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming</p></br>
+    		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming</p></br>
+		</div>
 		
 	</div>
 

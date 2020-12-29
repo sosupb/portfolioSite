15c6a4141a9fc1ebde2fdd4c0ad3d28dbf3f9276
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 16:48:45 -0800

    Added styling cards to about page.

--- presentation/pages/AboutPage.php
+++ presentation/pages/AboutPage.php
@@ -7,18 +7,18 @@
 
 	<div class="content">
 		<h1>About Me</h1>
-		<div style="margin-left: 3%;">
+		<div class="infoCard">
     		<h2>Technical Skills:</h2>
     		<b>Programming Languages - </b><p>C#, C++, Java, PHP, JavScript, SQL, HTML, CSS</p><br/>
     		<b>Development Tools - </b><p>Visual Studio, Eclipse, GIT, CI/CD, Cloud Development, MySQLWorkbench,</p><br/>
 		</div>
-		<div style="margin-left: 3%;">
+		<div class="infoCard">
     		<h2>Education:</h2>
     		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming</p>
     		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming</p>
     		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming</p>
 		</div>
-		<div style="margin-left: 3%;">
+		<div class="infoCard">
     		<h2>Previous Experience:</h2>
     		<b>Internship - IT Assistant, Meadowbrook Middle School</b>
     			<p>Assisted in multiple types of system debugging to help other faculty lessen their workload.

995e6cec6ed039a91efbb6a2b9fb7a3e4ba62ecf
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 16:35:47 -0800

    Added more information on the about page.

--- presentation/pages/AboutPage.php
+++ presentation/pages/AboutPage.php
@@ -14,9 +14,22 @@
 		</div>
 		<div style="margin-left: 3%;">
     		<h2>Education:</h2>
-    		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming</p></br>
-    		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming</p></br>
-    		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming</p></br>
+    		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming</p>
+    		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming</p>
+    		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming</p>
+		</div>
+		<div style="margin-left: 3%;">
+    		<h2>Previous Experience:</h2>
+    		<b>Internship - IT Assistant, Meadowbrook Middle School</b>
+    			<p>Assisted in multiple types of system debugging to help other faculty lessen their workload.
+				   Helping students learn how to navigate Windows and use their login to access academic resources.
+				   Servicing systems in the computer labs including physically cleaning them and clearing any non-essential programs that were installed on them.
+				</p>
+    		<b>Construction Foreman</b>
+    			<p>Delegate tasks to other employees based on their skill levels to speed up individual tasks.
+				   Communicating with the job site Owner Representatives to provide the desired end product.
+				   Making informed decisions to progress the work being done when potential problems threaten to halt the job completely.
+    			</p>
 		</div>
 		
 	</div>

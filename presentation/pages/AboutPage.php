d8bcdba8602b10c4722c75ea0f86d9af26a08b97
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-28 17:19:38 -0800

    Added dates to about page and added more styling to the cards.

--- presentation/pages/AboutPage.php
+++ presentation/pages/AboutPage.php
@@ -14,20 +14,20 @@
 		</div>
 		<div class="infoCard">
     		<h2>Education:</h2>
-    		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming</p>
-    		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming</p>
-    		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming</p>
+    		<b>Grand Canyon University</b><p>Bachelor of Science in Computer Programming<br/>2019 - preset</p>
+    		<b>San Diego Mesa College</b><p>Associates Degree in Computer Programming<br/>2015 - 2018</p>
+    		<b>San Diego Miramar College</b><p>Certificate of Performance in Computer Programming<br/>2018</p>
 		</div>
 		<div class="infoCard">
     		<h2>Previous Experience:</h2>
-    		<b>Internship - IT Assistant, Meadowbrook Middle School</b>
-    			<p>Assisted in multiple types of system debugging to help other faculty lessen their workload.
-				   Helping students learn how to navigate Windows and use their login to access academic resources.
+    		<b>Internship - IT Assistant, Meadowbrook Middle School<br/>2002</b>
+    			<p>Assisted in multiple types of system debugging to help other faculty lessen their workload.<br/>
+				   Helping students learn how to navigate Windows and use their login to access academic resources.<br/>
 				   Servicing systems in the computer labs including physically cleaning them and clearing any non-essential programs that were installed on them.
 				</p>
-    		<b>Construction Foreman</b>
-    			<p>Delegate tasks to other employees based on their skill levels to speed up individual tasks.
-				   Communicating with the job site Owner Representatives to provide the desired end product.
+    		<b>Construction Foreman<br/>2003 - present</b>
+    			<p>Delegate tasks to other employees based on their skill levels to speed up individual tasks.<br/>
+				   Communicating with the job site Owner Representatives to provide the desired end product.<br/>
 				   Making informed decisions to progress the work being done when potential problems threaten to halt the job completely.
     			</p>
 		</div>

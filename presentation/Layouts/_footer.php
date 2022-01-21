bf69df853224d28ddeccb4ab07f8310cb18afa40
Marc Teixeira <MTeixeira@my.gcu.edu>
2022-01-20 17:32:59 -0800

    Added dummy integer to site counter.

--- presentation/Layouts/_footer.php
+++ presentation/Layouts/_footer.php
@@ -9,7 +9,7 @@
 
 		<div class="fixedFooter">
 			<h2>Portfolio site for Marc D. Teixeira Jr. &emsp; Site up since: 12/22/2020</h2>
-			<h3>Unique visits: <?=$_SESSION['counter']?></h3>
+			<h3>Unique visits: 16</h3>
 		</div>
 	</body>
 </html>
